1696ac5fffcd398733fcbea5e4ab1bbfec30d0d2
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-06-17 21:24:28 +0800

    Make ch5 compilable, note of rgbd

--- ch5/rgbd/joinMap.cpp
+++ ch5/rgbd/joinMap.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 #include <opencv2/opencv.hpp>
 #include <boost/format.hpp>  // for formating strings
-#include <sophus/se3.hpp>
 #include <pangolin/pangolin.h>
+#include <sophus/se3.hpp>
 
 using namespace std;
 typedef vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>> TrajectoryType;
@@ -12,6 +12,29 @@ typedef Eigen::Matrix<double, 6, 1> Vector6d;
 // 在pangolin中画图，已写好，无需调整
 void showPointCloud(
     const vector<Vector6d, Eigen::aligned_allocator<Vector6d>> &pointcloud);
+
+std::string type2str(int type) {
+  std::string r;
+
+  uchar depth = type & CV_MAT_DEPTH_MASK;
+  uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch ( depth ) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+  }
+
+  r += "C";
+  r += (chans+'0');
+
+  return r;
+}
 
 int main(int argc, char **argv) {
     vector<cv::Mat> colorImgs, depthImgs;    // 彩色图和深度图
@@ -24,10 +47,18 @@ int main(int argc, char **argv) {
     }
 
     for (int i = 0; i < 5; i++) {
-        boost::format fmt("./%s/%d.%s"); //图像文件格式
-        colorImgs.push_back(cv::imread((fmt % "color" % (i + 1) % "png").str()));
-        depthImgs.push_back(cv::imread((fmt % "depth" % (i + 1) % "pgm").str(), -1)); // 使用-1读取原始图像
+        boost::format imgfmt("./%s/%d.%s"); //图像文件格式
+        colorImgs.push_back(cv::imread((imgfmt % "color" % (i + 1) % "png").str()));
+        /**
+         * https://docs.opencv.org/3.4/d8/d6a/group__imgcodecs__flags.html#ga61d9b0126a3e57d9277ac48327799c80
+         * IMREAD_UNCHANGED: If set, return the loaded image as is (with alpha channel, 
+         * otherwise it gets cropped). Ignore EXIF orientation. 
+         * https://docs.opencv.org/3.4/d6/d87/imgcodecs_8hpp.html
+         * cv::IMREAD_UNCHANGED = -1
+         **/
+        depthImgs.push_back(cv::imread((imgfmt % "depth" % (i + 1) % "pgm").str(), -1)); // 使用-1读取原始图像
 
+        // pose裡的每一行代表各相機的位姿，前三個是t，後四個是四元數(三虛+一實)
         double data[7] = {0};
         for (auto &d:data)
             fin >> d;
@@ -38,13 +69,29 @@ int main(int argc, char **argv) {
 
     // 计算点云并拼接
     // 相机内参 
+    // (cx, cy): 圖像中心
+    // (fx, fy): 相機焦距
     double cx = 325.5;
     double cy = 253.5;
     double fx = 518.0;
     double fy = 519.0;
     double depthScale = 1000.0;
+    /**
+     * https://www.cnblogs.com/Glucklichste/p/10912257.html
+     * Eigen中管理內存的方式和C++11不同,所以需要特別寫出來
+     **/ 
     vector<Vector6d, Eigen::aligned_allocator<Vector6d>> pointcloud;
     pointcloud.reserve(1000000);
+
+    // elemSize1: 矩陣中每個元素單個通道所占用的byte數
+    // color中每個元素單個通道占用1byte
+    cout << "color.elemSize1: " << colorImgs[0].elemSize1() << endl; //1
+    // depth中每個元素單個通道占用2byte
+    cout << "depth.elemSize1: " << depthImgs[0].elemSize1() << endl; //2
+    // color: 8U代表每個元素單個通道占用1byte,C3代表3個通道
+    cout << "color.type: " << type2str(colorImgs[0].type()) << endl; //8UC3
+    // color: 16U代表每個元素單個通道占用2byte,C1代表1個通道
+    cout << "depth.type: " << type2str(depthImgs[0].type()) << endl; //16UC1
 
     for (int i = 0; i < 5; i++) {
         cout << "转换图像中: " << i + 1 << endl;
@@ -56,13 +103,20 @@ int main(int argc, char **argv) {
                 unsigned int d = depth.ptr<unsigned short>(v)[u]; // 深度值
                 if (d == 0) continue; // 为0表示没有测量到
                 Eigen::Vector3d point;
+                // 注意depthScale = 1000,這裡是用除的
                 point[2] = double(d) / depthScale;
                 point[0] = (u - cx) * point[2] / fx;
                 point[1] = (v - cy) * point[2] / fy;
+                // 轉換矩陣T:相機座標系->世界座標系
                 Eigen::Vector3d pointWorld = T * point;
 
                 Vector6d p;
                 p.head<3>() = pointWorld;
+                /**
+                 * https://blog.csdn.net/dcrmg/article/details/52294259
+                 * step: 矩陣每一行的步長，每行元素個數*通道數*每個通道占幾個byte(elemSize1)
+                 * 計算u時應該是u*color.channels()*color.elemSize1?
+                 **/
                 p[5] = color.data[v * color.step + u * color.channels()];   // blue
                 p[4] = color.data[v * color.step + u * color.channels() + 1]; // green
                 p[3] = color.data[v * color.step + u * color.channels() + 2]; // red

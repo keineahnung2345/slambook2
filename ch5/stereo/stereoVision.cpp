86b046e6a6280a081f12ed4d1678415027eac406
keineahnung2345 <mimifasosofamire1123@gmail.com>
2021-06-18 22:57:52 +0800

    ch5 stereo note

--- ch5/stereo/stereoVision.cpp
+++ ch5/stereo/stereoVision.cpp
@@ -19,33 +19,54 @@ void showPointCloud(
 int main(int argc, char **argv) {
 
     // 内参
+    /**
+       * (X,Y,Z):相機坐標系下的三維點坐標
+       * (u,v):圖像上的坐標
+       * [u]    1  [fx 0 cx] [X]
+       * [v] = --- [0 fy cy] [Y]
+       * [1]    Z  [0  0  1] [Z]
+       * u = fx*nx+cx -> nx = (u-cx)/fx
+       * v = fy*ny+cy -> ny = (v-cy)/fy
+       **/
     double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157;
     // 基线
+    // z = fb/d
     double b = 0.573;
 
     // 读取图像
     cv::Mat left = cv::imread(left_file, 0);
     cv::Mat right = cv::imread(right_file, 0);
+    //https://docs.opencv.org/4.2.0/d2/d85/classcv_1_1StereoSGBM.html#adb7a50ef5f200ad9559e9b0e976cfa59
     cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(
         0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);    // 神奇的参数
     cv::Mat disparity_sgbm, disparity;
+    /**
+     * https://docs.opencv.org/4.2.0/d2/d6e/classcv_1_1StereoMatcher.html#a03f7087df1b2c618462eb98898841345
+     *  StereoSGBM compute 16-bit fixed-point disparity map
+     **/
     sgbm->compute(left, right, disparity_sgbm);
+    // ?
     disparity_sgbm.convertTo(disparity, CV_32F, 1.0 / 16.0f);
 
     // 生成点云
     vector<Vector4d, Eigen::aligned_allocator<Vector4d>> pointcloud;
 
     // 如果你的机器慢，请把后面的v++和u++改成v+=2, u+=2
+    // u是橫向，v是縱向
     for (int v = 0; v < left.rows; v++)
         for (int u = 0; u < left.cols; u++) {
+            // ?
             if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue;
 
             Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0); // 前三维为xyz,第四维为颜色
 
             // 根据双目模型计算 point 的位置
+            // 歸一化像平面上的坐標
             double x = (u - cx) / fx;
             double y = (v - cy) / fy;
+            // 由視差計算深度：z = fb/d
             double depth = fx * b / (disparity.at<float>(v, u));
+            // 歸一化像平面：(x,y) -> 世界坐標：(x * depth, y * depth)
             point[0] = x * depth;
             point[1] = y * depth;
             point[2] = depth;
@@ -53,6 +74,7 @@ int main(int argc, char **argv) {
             pointcloud.push_back(point);
         }
 
+    // ?
     cv::imshow("disparity", disparity / 96.0);
     cv::waitKey(0);
     // 画出点云
